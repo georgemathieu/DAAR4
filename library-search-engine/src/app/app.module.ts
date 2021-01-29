17800d325c1ca7f967881a25ea5468811815d17c
Pierre Gomez <pierre.machin@live.fr>
2021-01-29 18:32:47 +0100

    working form

--- library-search-engine/src/app/app.module.ts
+++ library-search-engine/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuicksearchComponent } from './quicksearch/quicksearch.component';
 import { AdvancedSearchComponent } from './advanced-search/advanced-search.component';
 import { AuthorSearchComponent } from './author-search/author-search.component';
 import { RouterModule, Routes } from '@angular/router';
+import{ HttpClientModule} from '@angular/common/http';
 import { DisplayResultsComponent } from './display-results/display-results.component';
 
 const appRoutes: Routes = [
   {path:'quicksearch', component: QuicksearchComponent},
   {path:'advancedsearch', component: AdvancedSearchComponent},
   {path:'authorsearch', component: AuthorSearchComponent},
+  {path: 'results', component: DisplayResultsComponent},
   {path:'', component: QuicksearchComponent}
 ]
 
@@ -25,6 +28,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- library-search-engine/src/app/quicksearch/quicksearch.component.ts
+++ library-search-engine/src/app/quicksearch/quicksearch.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quicksearch',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuicksearchComponent implements OnInit {
 
-  constructor() { }
+  shouldIDisplayResults: boolean = false;
+
+  results = [
+    {
+        "id": 547,
+        "title": "Project Trinity, 1945-1946",
+        "authors": [
+            "Rohrer, Steve",
+            "Maag, Carl R."
+        ],
+        "content": "https://www.gutenberg.org/ebooks/548.html.images",
+        "image": "https://www.gutenberg.org/cache/epub/548/pg548.cover.small.jpg"
+    },
+    {
+        "id": 683,
+        "title": "Worldwide Effects of Nuclear War: Some Perspectives",
+        "authors": [
+            "United States. Arms Control and Disarmament Agency"
+        ],
+        "content": "https://www.gutenberg.org/files/684/684-h/684-h.htm",
+        "image": "https://www.gutenberg.org/cache/epub/684/pg684.cover.medium.jpg"
+    }
+]
+
+  constructor(private router: Router) { }
+
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form.value);
+    this.shouldIDisplayResults = true;
+
   }
 
 }
